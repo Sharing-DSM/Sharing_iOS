4b2ba40cb9bcc92861757bbd26f838e9a58a9eae
Byung jin <bjcho1503@naver.com>
2023-09-24 00:23:20 +0900

    🎉 :: AppStepper

---
+++ Projects/AppFlow/Sources/AppStepper.swift
@@ -0,0 +1,17 @@
+import RxFlow
+import RxCocoa
+import Core
+
+public class AppStepper: Stepper {
+    public var steps = PublishRelay<Step>()
+
+    public init() { }
+
+    // TODO: 분기처리
+    /// - 자동 로그인 분기처리 부분임
+    /// - 나중에 분기처리 하기
+    public func readyToEmitSteps() {
+        steps.accept(SharingStep.onBoardingRequired)
+    }
+}
+
