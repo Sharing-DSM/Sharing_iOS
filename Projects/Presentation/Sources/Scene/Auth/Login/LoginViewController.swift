116debf8aa7b1b66c54117c01bbcb9f50e3606d1
Byung jin <bjcho1503@naver.com>
2023-09-25 08:50:25 +0900

    ⚡️ :: login뷰 파일 추가

---
+++ Projects/Presentation/Sources/Scene/Auth/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Presentation
+//
+//  Created by 조병진 on 2023/09/24.
+//  Copyright © 2023 com.Sharing. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
