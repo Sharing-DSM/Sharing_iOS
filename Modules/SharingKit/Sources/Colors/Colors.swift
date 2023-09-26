dce61c9294d4bd875db7399936c1f57331cf515d
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-26 23:38:57 +0900

    💄 :: SharingKit Colors

---
+++ Modules/SharingKit/Sources/Colors/Colors.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+extension UIColor {
+    //MARK: Blue500(main)
+    public static let main = UIColor(named: "Main")
+
+    //MARK: BLUE
+    public static let blue50 = UIColor(named: "Blue50")
+    public static let blue100 = UIColor(named: "Blue100")
+    public static let blue200 = UIColor(named: "Blue200")
+    public static let blue300 = UIColor(named: "Blue300")
+    public static let blue400 = UIColor(named: "Blue400")
+    public static let blue600 = UIColor(named: "Blue600")
+    public static let blue700 = UIColor(named: "Blue700")
+    public static let blue800 = UIColor(named: "Blue800")
+    public static let blue900 = UIColor(named: "Blue900")
+
+    //MARK: BLACK
+    public static let black50 = UIColor(named: "Black50")
+    public static let black100 = UIColor(named: "Black100")
+    public static let black200 = UIColor(named: "Black200")
+    public static let black300 = UIColor(named: "Black300")
+    public static let black400 = UIColor(named: "Black400")
+    public static let black500 = UIColor(named: "Black500")
+    public static let black600 = UIColor(named: "Black600")
+    public static let black700 = UIColor(named: "Black700")
+    public static let black800 = UIColor(named: "Black800")
+    public static let black900 = UIColor(named: "Black900")
+
+    //MARK: EXTRA
+    public static let check = UIColor(named: "Check")
+    public static let error = UIColor(named: "Error")
+    public static let focus = UIColor(named: "Focus")
+}
