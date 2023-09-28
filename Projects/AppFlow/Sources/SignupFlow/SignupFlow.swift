bb168c57de747ef12cd937574a24c9551c3eca4f
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-09-29 01:23:59 +0900

    ⚡️ :: SignupFlow

---
+++ Projects/AppFlow/Sources/SignupFlow/SignupFlow.swift
@@ -0,0 +1,30 @@
+import UIKit
+import Core
+import RxFlow
+import Presentation
+
+class SignupFlow: Flow {
+    public init() {}
+
+    var root: Presentable {
+        return rootPresentable
+    }
+
+    private lazy var rootPresentable = UINavigationController()
+
+    func navigate(to step: RxFlow.Step) -> RxFlow.FlowContributors {
+        guard let step = step as? SharingStep else { return .none }
+        switch step {
+        case .signupRequired:
+            return navigateToSignupView()
+        default:
+            return .none
+        }
+    }
+
+    private func navigateToSignupView() -> FlowContributors {
+        let signupViewController = SignupViewController()
+        rootPresentable.pushViewController(signupViewController, animated: false)
+        return .none
+    }
+}
