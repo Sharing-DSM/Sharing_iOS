3b77ca108f6f9db2102951c3c4fb74def31a1046
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-11-13 15:05:45 +0900

    ✨ :: ServiceDI

--- Projects/Data/Sources/ServiceDI.swift
+++ Projects/Data/Sources/ServiceDI.swift
@@ -5,19 +5,34 @@ public struct ServiceDI {
 
     public let loginUseCaseInject: LoginUseCase
     public let signupUseCaseInject: SignupUseCase
+
+    public let fetchUserProfileUseCaseInject: FetchUserProfileUseCase
+    public let patchUserProfileUseCaseInject: PatchUserProfileUseCase
+
+    //schedules
+    public let postSchedulesUseCaseInject: PostScheduleUseCase
 }
 
 extension ServiceDI {
     private static func resolve() -> ServiceDI {
         let authRepo = AuthRepositoryImpl()
+        let profileRepo = ProfileRepositoryImpl()
 
         // MARK: Auth관련 UseCase
         let loginUseCaseInject = LoginUseCase(repository: authRepo)
         let signupUseCaseInject = SignupUseCase(repository: authRepo)
 
+        let fetchUserProfileUseCaseInject = FetchUserProfileUseCase(repository: profileRepo)
+        let patchUserProfileUseCaseInject = PatchUserProfileUseCase(repository: profileRepo)
+
+        let postSchedulesUseCaseInject = PostScheduleUseCase(repository: profileRepo)
+
         return .init(
             loginUseCaseInject: loginUseCaseInject,
-            signupUseCaseInject: signupUseCaseInject
+            signupUseCaseInject: signupUseCaseInject,
+            fetchUserProfileUseCaseInject: fetchUserProfileUseCaseInject,
+            patchUserProfileUseCaseInject: patchUserProfileUseCaseInject,
+            postSchedulesUseCaseInject: postSchedulesUseCaseInject
         )
     }
 }
