8dde9a2485e43bbf467f391b8e9e819ce1772bec
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-11-13 15:03:44 +0900

    ✨ :: Schedule 관련 API 추가

--- Modules/AppNetwork/Sources/Profile/Remote/API/ProfileAPI.swift
+++ Modules/AppNetwork/Sources/Profile/Remote/API/ProfileAPI.swift
@@ -5,7 +5,13 @@ import Core
 public enum ProfileAPI {
     case fetchUserProfile
     case patchUserProfile(name: String, id: String, age: Int)
-    case postSchedule(title: String, date: Date)
+
+    case postSchedules(title: String, date: Date)
+    case fetchCompleteSchedules(id: String)
+    case fetchUnCompleteSchedule
+    case patchSchedules(id: String)
+    case deleteSchedules(id: String)
+    case completScheules(id: String)
 }
 
 extension ProfileAPI: TargetType {
@@ -17,25 +23,39 @@ extension ProfileAPI: TargetType {
         switch self {
         case .fetchUserProfile, .patchUserProfile:
             return "/users"
-        case .postSchedule:
-            return "/schedules/"
+        case .fetchCompleteSchedules:
+            return "/schedules/is-completed"
+        case .fetchUnCompleteSchedule:
+            return "/schedules"
+        case .postSchedules:
+            return "/schedules"
+        case .patchSchedules(let id):
+            return "schedules/\(id)"
+        case .deleteSchedules(let id):
+            return "/schedules/\(id)"
+        case .completScheules(let id):
+            return "schedules/check/\(id)"
         }
     }
     
     public var method: Moya.Method {
         switch self {
-        case .postSchedule:
+        case .postSchedules:
             return .post
-        case .patchUserProfile:
+        case .patchUserProfile, .patchSchedules:
             return .patch
-        case .fetchUserProfile:
+        case .fetchUserProfile, .fetchCompleteSchedules, .fetchUnCompleteSchedule:
             return .get
+        case .deleteSchedules:
+            return .delete
+        case .completScheules:
+            return .put
         }
     }
     
     public var task: Moya.Task {
         switch self {
-        case .postSchedule(let title, let date) :
+        case .postSchedules(let title, let date) :
             return .requestParameters(
                 parameters: [
                     "title": title,
