dcbec579ea22d6f91584896d54714e7c5483ae46
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-11-13 15:07:42 +0900

    ✨ :: UseCase들

---
+++ Projects/Domain/Sources/profile/UseCase/PostScheduleUseCase.swift
@@ -0,0 +1,15 @@
+import Foundation
+import RxSwift
+
+public class PostScheduleUseCase {
+
+    let repository: ProfileRepository
+
+    public init(repository: ProfileRepository) {
+        self.repository = repository
+    }
+
+    public func excute(title: String, date: Date) -> Completable {
+        return repository.postSchedules(title: title, date: date)
+    }
+}
