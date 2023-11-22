c2823495ad48b994e5a6d090ed43984d945e779d
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-11-22 23:09:50 +0900

    ⚡️ :: Entity들

--- Projects/Domain/Sources/profile/Entity/UserProfileEntity.swift
+++ Projects/Domain/Sources/profile/Entity/UserProfileEntity.swift
@@ -4,10 +4,12 @@ public struct UserProfileEntity {
     public let name: String
     public let accountId: String
     public let age: Int
+    public let profileImageURL: String
 
-    public init(name: String, accountId: String, age: Int) {
+    public init(name: String, accountId: String, age: Int, profileImageURL: String) {
         self.name = name
         self.accountId = accountId
         self.age = age
+        self.profileImageURL = profileImageURL
     }
 }
